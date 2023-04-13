b931e4c3602304cf355591be5f505238f535ab11
Saafiyah Patel <saafiyah@usc.edu>
2023-04-13 04:33:32 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t worker, size_t shift, vector<size_t>& shifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,8 +37,40 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // for(int i = 0; i < avail.size(); i++)
+    // {
+    //     vector<Worker_T> row;
+    //     sched.push_back(row); 
+    // }
+    sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t> shifts(avail[0].size(), 0);
 
-
-
+    if(helper(avail, dailyNeed, maxShifts, sched, 0, 0, shifts))
+    {
+        return true;
+    }
+    return false; 
 }
 
+bool helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t worker, size_t shift, vector<size_t>& shifts)
+{
+    if(avail.size() == worker)
+    {
+        return true; 
+    }
+    for(unsigned int i = 0; i < avail[0].size(); i++)
+    {
+        if(shifts[i] < maxShifts && avail[worker][i] == 1)
+        {
+            shifts[i] = shifts[i] + 1; 
+            sched[worker][shift] = i; 
+            if(helper(avail, dailyNeed, maxShifts, sched, (shift + 1)/dailyNeed + worker, (shift + 1) % dailyNeed, shifts))
+            {
+                return true;
+            }
+            shifts[i] = shifts[i] - 1; 
+            sched[worker][shift] = INVALID_ID; 
+        }
+    }
+    return false; 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
-
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +21,43 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+	set<string> retval; //set of strings with valid words 
+	if(in.find('-') == string::npos) // if in contains no dashes 
+	{
+		if(dict.find(in) != dict.end()) // if in is in dicitionary
+		{
+			retval.insert(in); // add to string set
+		}
+		return retval; 
+	}
+	if(!floating.empty()) // if not empty 
+	{
+		int found = 0; 
+		char hold = floating.front();
+		while(in.find('-', found) != string::npos) // go through every dash in in
+		{
+			string tempIn = in; 
+			tempIn.replace(tempIn.find('-', found), 1, 1, hold);
+			found = in.find('-', found) + 1; 
+			string tempFloat = floating;
+			tempFloat.erase(0, 1); 
+			set<string> result = wordle(tempIn, tempFloat, dict);
+      retval.insert(result.begin(), result.end());
+		}
+	}
+	else
+	{
+		for(int i = 0; i < 26; i++)
+		{
+			string tempIn = in; 
+			char hold = 'a' + i; 
+			tempIn.replace(tempIn.find('-'), 1, 1, hold);
+			set<string> result = wordle(tempIn, floating, dict);
+      retval.insert(result.begin(), result.end());
+		}
+	}
+	return retval; 
 }
 
 // Define any helper functions here
+
